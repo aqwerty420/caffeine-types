f942ab0ac00de25027ca6d2e57f7e2080449775b
AQ <aqwerty420@protonmail.com>
2023-09-27 15:01:23 +0200

    feat: some spell book typing

--- types/spellBook.d.ts
+++ types/spellBook.d.ts
@@ -1,3 +1,35 @@
+/**
+ * Represents a caffeine spell book.
+ */
 interface CaffeineSpellBook {
+  /**
+   * Creates a new spell book object.
+   *
+   * @returns A new spell book object.
+   */
+  New(): CaffeineSpellBook;
+
+  /**
+   * Gets a spell by its id.
+   *
+   * @param spellId - The numeric id of the spell.
+   *
+   * @returns The spell object.
+   */
   GetSpell(spellId: number): CaffeineSpell;
+
+  /**
+   * Gets spells by their ids.
+   *
+   * @param spellIds - The numeric ids of the spells.
+   *
+   * @returns The spell objects.
+   */
+  GetSpells(spellIds: number[]): CaffeineSpell[];
+
+  // TODO: GetList
+
+  // TODO: GetSpellByName
+
+  // TODO: GetIfRegistered
 }
