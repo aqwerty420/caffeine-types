4a6b4ea02d32d7da9960b3e750bccde44dfc91f6
AQ <aqwerty420@protonmail.com>
2023-09-27 14:00:04 +0200

    feat: most of unit api

--- types/unit.d.ts
+++ types/unit.d.ts
@@ -349,4 +349,353 @@ interface CaffeineUnit {
    * @returns True if the unit is interruptible and cast/channel has reached the specified percent, false otherwise.
    */
   IsInterruptibleAt(percent: number, ignoreInterruptible?: boolean): boolean;
+
+  /**
+   * Gets the number of enemies within the specified range of the unit.
+   *
+   * @remarks Result is cached for O.5 seconds.
+   *
+   * @param range - The range to check.
+   *
+   * @returns The number of enemies within the specified range of the unit.
+   */
+  GetEnemies(range: number): number;
+
+  /**
+   * Gets the number of melee attackers.
+   
+   * @returns The number of melee attackers.
+   */
+  GetMeleeAttackers(): number;
+
+  /**
+   * Gets the number of friendly units within the specified range of the unit and with an HP below the specified percent.
+   *
+   * @param distance - The maximal distance at which the unit can be.
+   * @param percent - The maximal HP percent the unit can have.
+   *
+   * @returns The number of friendly units corresponding to the specified criteria.
+   */
+  GetPartyHPAround(distance: number, percent: number): number;
+
+  /**
+   * Checks if the unit is moving.
+   *
+   * @remarks This function is using unit speed to determine if the unit is moving.
+   *
+   * @returns True if the unit is moving, false otherwise.
+   */
+  IsMoving(): boolean;
+
+  /**
+   * Checks if the unit is moving at all.
+   *
+   * @remarks This function is using object movment flags to determine if the unit is moving.
+   *
+   * @returns True if the unit is moving at all, false otherwise.
+   */
+  IsMovingAtAll(): boolean;
+
+  /**
+   * Gets the unit's combo points.
+   *
+   * @returns The unit's combo points.
+   */
+  GetComboPoints(): number;
+
+  /**
+   * Gets the unit's max combo points.
+   *
+   * @returns The unit's max combo points.
+   */
+  GetComboPointsMax(): number;
+
+  /**
+   * Gets the unit's combo points deficit.
+   *
+   * @returns The unit's combo points deficit.
+   */
+  GetComboPointsDeficit(): number;
+
+  /**
+   * Checks if the unit is same as another unit.
+   *
+   * @param unit - The other unit to check.
+   *
+   * @returns True if the unit is the same as the other unit, false otherwise.
+   */
+  IsUnit(unit: CaffeineUnit): boolean;
+
+  /**
+   * Checks if the given unit is being tanked by the unit.
+   *
+   * @param unit - The other unit to that need to be tanked by.
+   *
+   * @returns True if the unit is the main target of the other unit, false otherwise.
+   */
+  IsTanking(unit: CaffeineUnit): boolean;
+
+  /**
+   * Checks if the unit is facing the given unit.
+   *
+   * @param unit - The other unit that need to be faced.
+   *
+   * @returns True if the unit is facing the other unit, false otherwise.
+   */
+  IsFacing(unit: CaffeineUnit): boolean;
+
+  /**
+   * Checks if the unit is behind the given unit.
+   *
+   * @param unit - The other unit to check.
+   *
+   * @returns True if the unit is behind the given unit, false otherwise.
+   */
+  IsBehind(unit: CaffeineUnit): boolean;
+
+  /**
+   * Checks if the unit is in front of the given unit.
+   *
+   * @param unit - The other unit to check.
+   *
+   * @returns True if the unit is in front of the given unit, false otherwise.
+   */
+  IsInfront(unit: CaffeineUnit): boolean;
+
+  // TODO: documentation
+  GetMeleeBoost(): number;
+
+  // TODO: documentation
+  InMelee(): boolean;
+
+  /**
+   * Gets the unit's id.
+   *
+   * @returns The unit's id.
+   */
+  GetID(): number;
+
+  /**
+   * Checks if the unit is in the player's party.
+   *
+   * @returns True if the unit is in the player's party, false otherwise.
+   */
+  IsInParty(): boolean;
+
+  // TODO: documentation
+  LinearRegression(
+    time: number,
+    percent: number,
+  ): LuaMultiReturn<[number, number]>;
+
+  /**
+   * Gets the unit's predicted health in the specified time.
+   *
+   * @param time - The time in seconds.
+   *
+   * @returns The unit's predicted health in the specified time.
+   */
+  PredictHealth(time: number): number;
+
+  /**
+   * Gets the predicted time (in seconds) at which the unit's health will reach the specified percent.
+   *
+   * @param percent - The percent the unit's health will reach.
+   *
+   * @returns The predicted time (in seconds) at which the unit's health will reach the specified percent.
+   */
+  PredictTime(percent: number): number;
+
+  // TODO: improve documentation, is it global or per unit?
+  /**
+   * Starts the TTD ticker.
+   */
+  StartTTDTicker(): void;
+
+  /**
+   * Gets the unit's time to die.
+   *
+   * @returns The unit's time till death.
+   */
+  TimeToDie(): number;
+
+  // TODO: documentation
+  GetCombatTime(): number;
+
+  // TODO: documentation
+  SetLastCombatTime(time: number): void;
+
+  // TODO: documentation
+  InCombatOdds(): number;
+
+  /**
+   * Gets the unit's GCD.
+   *
+   * @returns The unit's GCD.
+   */
+  GetGCD(): number;
+
+  /**
+   * Gets the unit's max GCD.
+   *
+   * @returns The unit's max GCD.
+   */
+  GetMaxGCD(): number;
+
+  /**
+   * Checks if the unit is stealthed.
+   *
+   * @returns True if the unit is stealthed, false otherwise.
+   */
+  IsStealthed(): boolean;
+
+  /**
+   * Gets the unit's swing timer.
+   *
+   * @returns The unit's swing timer.
+   */
+  GetSwingTimers(): LuaMultiReturn<[number, number]>;
+
+  /**
+   * Starts watching for weapon swings.
+   */
+  WatchForSwings(): void;
+
+  /**
+   * Checks if the unit is mounted.
+   *
+   * @returns True if the unit is mounted, false otherwise.
+   */
+  IsMounted(): boolean;
+
+  /**
+   * Checks if the unit is outdoors.
+   *
+   * @returns True if the unit is outdoors, false otherwise.
+   */
+  IsOutdoors(): boolean;
+
+  /**
+   * Checks if the unit is indoors.
+   *
+   * @returns True if the unit is indoors, false otherwise.
+   */
+  IsIndoors(): boolean;
+
+  /**
+   * Checks if the unit is swimming.
+   *
+   * @returns True if the unit is swimming, false otherwise.
+   */
+  IsSubmerged(): boolean;
+
+  /**
+   * Checks if the unit is not swimming.
+   *
+   * @returns True if the unit is not swimming, false otherwise.
+   */
+  IsDry(): boolean;
+
+  /**
+   * Gets the unit's stagger amount.
+   *
+   * @returns The unit's stagger amount.
+   */
+  GetStagger(): number;
+
+  /**
+   * Gets the unit's max stagger amount.
+   *
+   * @returns The unit's max stagger amount.
+   */
+  GetStaggerPercent(): number;
+
+  /**
+   * Gets the unit's power regen.
+   *
+   * @returns The unit's power regen.
+   */
+  GetPowerRegen(): number;
+
+  /**
+   * Gets the unit's staggered health relation.
+   *
+   * @returns The unit's staggered health relation.
+   */
+  GetStaggeredHealth(): number;
+
+  /**
+   * Gets the unit's combat reach.
+   *
+   * @returns The unit's combat reach.
+   */
+  GetCombatReach(): number;
+
+  /**
+   * Gets the unit's combat distance.
+   *
+   * @remarks it's equals to: (distance - combat reach (realized distance))
+   *
+   * @param target - The unit to get the combat distance to.
+   *
+   * @returns The unit's combat distance.
+   */
+  GetCombatDistance(target: CaffeineUnit): number;
+
+  /**
+   * Checks if the unit is within the distance to the given unit while considering combat reach.
+   *
+   * @remarks it's equals to: (combat reach + distance)
+   *
+   * @param target - The unit to check.
+   * @param distance - The distance to check.
+   *
+   * @returns True if the unit is within the specified combat distance to the given unit, false otherwise.
+   */
+  IsWithinCombatDistance(target: CaffeineUnit, distance: number): boolean;
+
+  /**
+   * Checks if the unit is within the specified distance to the given unit.
+   *
+   * @param target - The unit to check.
+   * @param distance - The distance to check.
+   *
+   * @returns True if the unit is within the specified distance to the given unit (consider combat reach), false otherwise.
+   */
+  IsWithinDistance(target: CaffeineUnit, distance: number): boolean;
+
+  /**
+   * Gets the unit's angle to the given unit.
+   *
+   * @param target - The unit to get the angle to.
+   *
+   * @returns The unit's angle to the given unit.
+   */
+  GetAngle(target: CaffeineUnit): number;
+
+  /**
+   * Gets the unit's rotation.
+   *
+   * @returns The unit's rotation.
+   */
+  GetFacing(): number;
+
+  /**
+   * Checks if target is within a arc around the unit (angle, distance) accounting for a rotation of self.
+   *
+   * @param target - The unit to check.
+   * @param angle - The angle to check.
+   * @param distance - The distance to check.
+   * @param rotation - The rotation to check.
+   *
+   * @returns True if the unit is within the specified arc around the given unit, false otherwise.
+   */
+  IsWithinCone(
+    target: CaffeineUnit,
+    angle: number,
+    distance: number,
+    rotation: number,
+  ): boolean;
+
+  // TODO: GetEmpoweredStage
 }
